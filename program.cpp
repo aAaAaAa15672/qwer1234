fba20da72b1d9d0c90269be2d86de485cd136a60
aAaAaAa15672 <71511438+aAaAaAa15672@users.noreply.github.com>
2023-04-27 09:42:02 +0000

    asdfasdfasdf

--- program.cpp
+++ program.cpp
@@ -1,5 +1,14 @@
 #include "graphics.h"
 #include "picture.hpp"
+
+void house(int x, int y, int h){
+   setfillstyle(SOLID_FILL, COLOR(128,64,48));
+   bar(x,y,x+100,y+100);
+   //treug(x-50,y,200, RED);
+   setfillstyle(BKSLASH_FILL, WHITE);
+   bar(x+33, y+33, x+66, y+66);
+}
+
 int main()
 { initwindow(800,600);
  house(); // дом
